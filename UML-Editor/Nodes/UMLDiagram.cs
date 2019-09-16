baf45e4116834da1d740597a90317255ecd37f40
Roman Španko <romanspankojr@gmail.com>
2019-09-17 00:22:16 +0200

    Added UMLDiagramNode

--- UML-Editor/Nodes/UMLDiagram.cs
+++ UML-Editor/Nodes/UMLDiagram.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UML_Editor.Others;
 using UML_Editor.Rendering;
+using UML_Editor.Enums;
 
 namespace UML_Editor.Nodes
 {
@@ -16,11 +17,15 @@ namespace UML_Editor.Nodes
         public abstract int Height { get; set; }
         public bool Resize { get; set; }
         public List<IHitbox> TriggerAreas { get; set; }
+        public List<PropertyNode> Properties { get; set; } = new List<PropertyNode>();
+        public List<MethodNode> Methods { get; set; } = new List<MethodNode>();
+        public Modifiers Modifiers { get; set; }
 
         public void ForceResize(int width)
         {
         }
-
+        public abstract void AddProperty();
+        public abstract void AddMethod();
         public abstract List<INode> GetChildren();
 
         public abstract void Render(Renderer renderer);
